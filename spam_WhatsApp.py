36492410716c378e7ff363130a618d79a7feee14
01sneha <sneha01mahajan@gmail.com>
2020-11-07 17:39:56 +0530

    Initial commit

--- spam_WhatsApp.py
+++ spam_WhatsApp.py
@@ -1,43 +1,10 @@
-# import time
-# import pyautogui as pya
-#
-#
-# class cordinate:
-#     emptyMessage = (173, 700)
-#     sendChat = (450, 700)
-#     pya.size()
-#
-#
-# def clicktochat():
-#     pya.click(cordinate.emptyMessage)
-#
-#
-# def typetochat():
-#     print("Hi")
-#
-#
-# def detailtext():
-#     print("Sup?")
-#
-#
-# def sendtochat():
-#     pya.click(cordinate.sendChat)
-#     print("I am bored!!!")
-#
-#
-# while True:
-#     clicktochat()
-#     typetochat()
-#     detailtext()
-#     sendtochat()
-#     time.sleep(0.5)
-
 import time
 import pyautogui as pya
 
 
 class cordinate:
-    emptyMessage = [90, 110]  # set the coordinates accordingly- to click on the bar
+    emptyMessage = (173, 700)
+    sendChat = (450, 700)
     pya.size()
 
 
@@ -46,16 +13,22 @@ def clicktochat():
 
 
 def typetochat():
-    pya.write('Hi')  # type text
-    pya.press('enter')  # hit enter
-    pya.write("What's up?")
-    pya.press('enter')
-    pya.write("I am bored!!!")
-    pya.press('enter')
+    print("Hi")
 
 
-numberOfTimes = 10
-while numberOfTimes:
+def detailtext():
+    print("Sup?")
+
+
+def sendtochat():
+    pya.click(cordinate.sendChat)
+    print("I am bored!!!")
+
+
+while True:
     clicktochat()
     typetochat()
+    detailtext()
+    sendtochat()
     time.sleep(0.5)
+
